6f37d642841309ae07106892c60f15a67e32ed31
Vinicius Rocha Schiffler <viniciusrshiffler@gmail.com>
2022-09-26 22:30:06 -0300

    Aula 06

---
+++ Aula 06/pages/repos/script.js
@@ -0,0 +1,26 @@
+var user = JSON.parse(localStorage.getItem("user"))
+console.log(user)
+
+document.getElementById('image').src = user.avatar_url
+document.getElementById('name').innerText = user.name
+
+async function pegarRepos() {
+ var response = await fetch(user.repos_url)
+ var repos = await response.json()
+
+var main = document.getElementById('main')
+
+ for (var i = 0; i < repos.length; i++) {
+    console.log(repos[i])
+
+    main.innerHTML += `
+    <div class="card_container">
+        <strong>Clone-Tesla</strong>
+        <span>JavaScript</span>
+    </div>
+    `
+ }
+ 
+}
+
+pegarRepos()
